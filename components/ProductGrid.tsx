20a230d7235c9d0cdf6e2533f587cd8ce28a01ac
EDWIN GITHURE <davies036@gmail.com>
2025-06-16 08:18:54 +0300

    Sanity configration

--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -1,17 +1,28 @@
-"use client";
+// "use client";
 
 // import React, { useState } from 'react'
-import HomeTabbar from './HomeTabbar';
+// import HomeTabbar from './HomeTabbar';
+
+// const ProductGrid = () => {
+//   const [products, setProducts] = useState([]);
+//   const [loading, setLoading] = useState(false);
+//   const [selectedTab, setSelectedTab] = useState("");
+//   return (
+
+//     <div>
+//       <HomeTabbar />
+//     </div>
+//   )
+// }
+
+// export default ProductGrid
+
+import React from 'react'
+import HomeTabbar from './HomeTabbar'
 
 const ProductGrid = () => {
-  // const [products, setProducts] = useState([]);
-  // const [loading, setLoading] = useState(false);
-  // const [selectedTab, setSelectedTab] = useState("");
   return (
-
-    <div>
-      <HomeTabbar />
-    </div>
+    <div><HomeTabbar /></div>
   )
 }
 
