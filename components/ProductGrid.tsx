1b344af17751d161a1e5a9e735b73d500091275d
EDWIN GITHURE <davies036@gmail.com>
2025-06-16 08:07:48 +0300

    Sanity configration

--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -4,9 +4,9 @@ import React, { useState } from 'react'
 import HomeTabbar from './HomeTabbar';
 
 const ProductGrid = () => {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [selectedTab, setSelectedTab] = useState("");
+  // const [products, setProducts] = useState([]);
+  // const [loading, setLoading] = useState(false);
+  // const [selectedTab, setSelectedTab] = useState("");
   return (
 
     <div>
