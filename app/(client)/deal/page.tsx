6287febf1657c835a55717676317d766ec9e5cd7
EDWIN GITHURE <davies036@gmail.com>
2025-06-27 22:36:32 +0300

    dubugging deployment errors

--- app/(client)/deal/page.tsx
+++ app/(client)/deal/page.tsx
@@ -14,7 +14,7 @@ const DealPage = async () => {
         </Title>
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-2.5">
           {products?.map((product) => (
-            //@ts-expect-error
+            // @ts-expect-error Product type mismatch is expected here
             <ProductCard key={product?._id} product={product} />
           ))}
         </div>
