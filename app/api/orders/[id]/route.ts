904f2ca88662c5b48ae73a074cd236cf48d35464
EDWIN GITHURE <davies036@gmail.com>
2025-07-02 16:25:53 +0300

    fixing vercel deployment

--- app/api/orders/[id]/route.ts
+++ app/api/orders/[id]/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { backendClient } from "@/sanity/lib/backendClient";
 import { sendOrderStatusEmail, OrderStatusEmailData } from "@/lib/email";
 
