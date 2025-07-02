9362bd5c40ff43f679683d6234cf3db6efe3a990
EDWIN GITHURE <davies036@gmail.com>
2025-07-02 19:41:34 +0300

    fixing depoyment issues on orders

--- app/api/orders/[id]/route.ts
+++ app/api/orders/[id]/route.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextResponse } from "next/server";
 import { backendClient } from "@/sanity/lib/backendClient";
 import { sendOrderStatusEmail, OrderStatusEmailData } from "@/lib/email";
