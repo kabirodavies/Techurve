1e5e227d0e2e82b5e47f0b56944105a30b76e62e
EDWIN GITHURE <davies036@gmail.com>
2025-07-20 23:31:17 +0300

    vercel deployment 2

--- app/(client)/brand/[slug]/page.tsx
+++ app/(client)/brand/[slug]/page.tsx
@@ -1,10 +1,11 @@
 import { BrandPageClient } from "./BrandPageClient";
 import { getProductsByBrandSlug, getAllBrands } from "@/sanity/queries";
+import { Product } from "@/sanity.types";
 
 // Server wrapper to fetch data and pass as props
 export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
   const { slug } = await params;
-  const products = (await getProductsByBrandSlug(slug)).map((product: any) => ({
+  const products = (await getProductsByBrandSlug(slug)).map((product: Product) => ({
     ...product,
     brand: product.brand === null ? undefined : product.brand,
   }));
